b6efc424fc2589fffeca442406008a3493e2847c
Aman <amankumar94719@gmail.com>
2024-02-14 20:48:11 +0530

    Use Onclick Outside

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import TicTacToe from './components/tic-tac-toe';
 import TreeView from './components/tree-view';
 import menus from './components/tree-view/data';
 import UseFetchHookTest from './components/use-fetch/test';
+import UseOnclickOutsideTest from './components/use-outside-click/test';
 
 function App() {
   return (
@@ -71,6 +72,9 @@ function App() {
 
          {/* useFetch - custom Hook */}
          <UseFetchHookTest/>
+
+         {/* Use Onclick Outside Hook Test */}
+         <UseOnclickOutsideTest/>
 
     </div>
   );
